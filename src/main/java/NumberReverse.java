5a1edc65cec26e581562c60f8a82675ed909e303
keerthi <keerthi.rapolu@yahoo.com>
2021-10-05 21:20:49 +0100

    number reverse

--- src/main/java/NumberReverse.java
+++ src/main/java/NumberReverse.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
-
 public class NumberReverse {
+
+     static int num;
+
     public static void main(String[] args) {
         NumberReverse nb=new NumberReverse();
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        num= sc.nextInt();
+
         nb.sbMethod();
         nb.sblMethod();
-        Scanner sc=new Scanner(System.in);
-
-        System.out.println("enter a number");
-       int num= sc.nextInt();
-
 
     }
     //either we can pass int when obj is created or append to the obj after it is created.
-    
+
     public void sbMethod(){
         int x=234;
-        StringBuffer sb=new StringBuffer(String.valueOf(x));
+        StringBuffer sb=new StringBuffer(String.valueOf(num));
       // StringBuffer newSB=sb.append(x);
      // StringBuffer rev= newSB.reverse();
         StringBuffer rev1= sb.reverse();
